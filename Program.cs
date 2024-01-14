1594ae2031313c05cb00f4ebcf874633cf1068a5
Lais Silva Ximenes <69364171+laissilvaximenes@users.noreply.github.com>
2024-01-14 01:46:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main()
+    {
+        // Teste com Iphone
+        Iphone iphone = new Iphone("123456789", "iPhone 12", "ABC123", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        // Teste com Nokia
+        Nokia nokia = new Nokia("987654321", "Nokia 3310", "XYZ456", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+    }
+}
